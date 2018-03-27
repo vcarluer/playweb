b9e1983a17855a4e739d0bcfa599bdb7a1085a53
Vincent Carluer <vince@bitfu.io>
2018-03-27 11:55:22 +0200

    prod ready

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
-export FLASK_APP=playweb.py
-export FLASK_DEBUG=1
-flask run
+export FLASK_APP=play.py
+if [[ "$1" == "--prod" ]]; then
+	export FLASK_DEBUG=0
+	HOST='127.0.0.1'
+	PORT=4246
+	nohup='nohup '
+else
+	export FLASK_DEBUG=1
+	HOST='127.0.0.1'
+	PORT=5000
+	nohup=''
+fi
+
+${nohup}python3 -m flask run --port $PORT --host $HOST
