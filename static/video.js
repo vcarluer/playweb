406e80a6d196ff10053486b41ed23b918e7d0784
Vincent Carluer <vince@bitfu.io>
2018-04-07 13:15:24 +0200

    Saving player position

---
+++ static/video.js
@@ -0,0 +1,33 @@
+(function() {
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+var lastSavedTime = 0;
+var saveFrequency = 10; // in milliseconds
+
+function init() {
+	var player = document.getElementById('player');
+	player.ontimeupdate = function() {
+		newTime = player.currentTime;
+		if (Math.abs(newTime - lastSavedTime) > saveFrequency) {
+			console.log('saving position ' + newTime);
+			lastSavedTime = newTime;
+			localStorage.setItem('currentTime', newTime);
+		}
+	};
+
+	var prevTime = localStorage.getItem('currentTime');
+	if (prevTime) {
+		player.currentTime = prevTime;
+		lastSavedTime = prevTime;
+	}
+}
+
+ready(init);
+
+}());
